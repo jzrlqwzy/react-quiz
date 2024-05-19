6f6d95e97e80bf1da186e5a5ac2b0101c4eca779
lei <littlesomerest@gmail.com>
2024-05-19 13:16:11 +0800

    receive data from local api

--- src/App.js
+++ src/App.js
@@ -7,10 +7,39 @@
 //     </div>
 //   );
 // }
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initalState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unknown type");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initalState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      // .then((data) => console.log(data))
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      // .catch((err) => console.error(err.message));
+      .catch(() => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
