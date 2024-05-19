f119258f9cc54a5ab556e7d8c38ebc37279eb8b2
lei <littlesomerest@gmail.com>
2024-05-19 14:07:41 +0800

    display question

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,12 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import Options from "./Options";
 
 const initalState = {
   questions: [],
-  // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -27,7 +28,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initalState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initalState
+  );
   const numQuestions = questions.length;
 
   useEffect(function () {
@@ -49,7 +53,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
