d8c59c267bdc0c8f57c012b0edab55653b3f61fe
lei <littlesomerest@gmail.com>
2024-05-19 12:46:32 +0800

    Main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import DateCounter from "./DateCounter.js";
+// import DateCounter from "./DateCounter.js";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <DateCounter />
+//     </div>
+//   );
+// }
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
 }
